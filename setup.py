ce12d22dfd16e3e613cae41ca58546250d2346af
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2021-10-05 09:58:46 +0200

    use setup.cfg

--- setup.py
+++ setup.py
@@ -1,20 +1,2 @@
-from setuptools import setup, find_packages
-
-if __name__ == "__main__":
-    setup(
-        name="pytest-external-blockers",
-        description=(
-            "a special outcome for tests "
-            "that are blocked for external reasons"),
-        use_scm_version=True,
-        url="https://github.com/RonnyPfannschmidt-RedHat/pytest-external-blockers",
-        packages=find_packages("src"),
-        package_dir={'': 'src'},
-        setup_requires=["setuptools-scm"],
-        install_requires=["pytest"],
-        entry_points={
-            'pytest11': [
-                'external-blockers = pytest_external_blockers'
-            ],
-        }
-    )
+from setuptools import setup
+setup()
